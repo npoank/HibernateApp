20cfa62dacbde9f3c860a592f79ab19b4ce6f1cf
Kulbicki Andrey <kubl-ml@mail.ru>
2022-12-13 11:37:56 +0300

    generated value strategy -- sequence

--- src/main/java/org/example/model/Person.java
+++ src/main/java/org/example/model/Person.java
@@ -8,7 +8,8 @@ public class Person {
 
     @Id
     @Column(name = "id")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_gen_person")
+    @SequenceGenerator(name = "seq_gen_person", sequenceName = "person_id_seq", allocationSize = 1)
     private int id;
 
     @Column(name = "name")
